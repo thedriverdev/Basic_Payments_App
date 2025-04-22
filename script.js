a72b8c5ccd109aafb846b6f5471416631dc2fe4b
Alfred Mamman Odey <alfredmodey@gmail.com>
2025-04-22 18:23:16 +0100

    Fourth commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const nameInput = document.querySelector(".name-input");
+const passwordInput = document.querySelector(".password-input");
+const confirmPasswordInput = document.querySelector(".confirm-password-input");
+const signUpButton = document.querySelector(".sign-up-button");
+const signUpFeedback = document.querySelector(".sign-up-feedback");
+const numOfAccountsDisplay = document.querySelector(".num-of-accounts");
+
+let availableAccounts = JSON.parse(localStorage.getItem("availableAccounts")) || JSON.stringify(defaultAccount);
+
+function signUp() {
+
+  let userDetails = {
+    userName: nameInput.value,
+    userPassword: passwordInput.value,
+    userBalance: 0,
+    userAccount: Math.floor(Math.random() * 9000) + 1000
+  };
+
+  if (userDetails.userName.trim() !== null && userDetails.userPassword !== null && confirmPasswordInput.value === userDetails.userPassword) {
+
+    if (availableAccounts.find((availableAccount)=>{return availableAccount.userName === userDetails.userName})) {
+      signUpFeedback.textContent = "Username already taken!";
+    } else {
+
+      if (userDetails.userName === "Alfie") {
+        userDetails.userBalance = 500;
+        availableAccounts.push(userDetails);
+        localStorage.setItem("availableAccounts", JSON.stringify(availableAccounts));
+        localStorage.setItem("numOfAccounts", availableAccounts.length);
+  
+        nameInput.value = "";
+        passwordInput.value = "";
+        confirmPasswordInput.value = "";
+        let numOfAccounts = localStorage.getItem("numOfAccounts");
+        numOfAccountsDisplay.textContent = `Number of available accounts: ${numOfAccounts}`;
+  
+        signUpFeedback.textContent = `Sign up successful, ${userDetails.userName}`;
+      } else {
+        availableAccounts.push(userDetails);
+        localStorage.setItem("availableAccounts", JSON.stringify(availableAccounts));
+        localStorage.setItem("numOfAccounts", availableAccounts.length);
+  
+        nameInput.value = "";
+        passwordInput.value = "";
+        confirmPasswordInput.value = "";
+        let numOfAccounts = localStorage.getItem("numOfAccounts");
+        numOfAccountsDisplay.textContent = `Number of available accounts: ${numOfAccounts}`;
+  
+        signUpFeedback.textContent = `Sign up successful, ${userDetails.userName}`;
+      }
+
+    }
+
+    
+  } else {
+    signUpFeedback.textContent = "Make sure you have entered a name, and your passwords match.";
+  }
+
+} signUpButton.onclick = signUp;
---
+++ sign-in.js
@@ -0,0 +1,108 @@
+// Sign in
+
+const signInHeader = document.getElementById("sign-in-header");
+const nameInput = document.querySelector(".name-input");
+const passwordInput = document.querySelector(".password-input");
+const signInButton = document.querySelector(".sign-in-button");
+const signInFeedback = document.querySelector(".sign-in-feedback");
+const userAccountInfo = document.querySelector(".user-account-info");
+const transactionContainer = document.querySelector(".transaction-container");
+
+signInHeader.textContent = "Sign in to Moola.com";
+
+function signIn() {
+
+  let availableAccounts = JSON.parse(localStorage.getItem("availableAccounts"));
+
+  let matchedUser = availableAccounts.find((userDetails) => { return nameInput.value === userDetails.userName && passwordInput.value === userDetails.userPassword});
+
+  if (matchedUser) {
+
+    signInHeader.style.display = "none";
+    
+    signInFeedback.innerHTML = `Sign in successful!
+    Welcome, ${matchedUser.userName}! <button class="sign-out-button" onclick="signOut();">Sign out</button>`;
+
+    localStorage.setItem("loggedInUser", matchedUser.userName);
+
+    nameInput.style.display = "none";
+    passwordInput.style.display = "none";
+    signInButton.style.display = "none";
+
+    userAccountInfo.innerHTML = `<img src="default.jpeg" alt="Profile photo of ${matchedUser.userName}" /><br>`;
+
+    transactionContainer.innerHTML = `
+        <input class="destination-account-name" type="text" placeholder="Enter name of recepient">
+        <input class="number-input" type="number" placeholder="Enter amount">
+        <button class="send-button">Send money</button>
+        <p class="feedback-display"></p>`;
+
+        const accountBalanceDisplay = document.querySelector(".account-balance-display");
+        const accountNumberDisplay = document.querySelector(".account-number-display");
+        const destinationAccountName = document.querySelector(".destination-account-name");
+
+        accountBalanceDisplay.innerHTML = `Balance: ₦${parseInt(matchedUser.userBalance)}`;
+        accountNumberDisplay.innerHTML = `Number: ${matchedUser.userAccount}`;
+
+        const numberInput = document.querySelector(".number-input");
+        const sendButton = document.querySelector(".send-button");
+        const feedbackDisplay = document.querySelector(".feedback-display");
+
+
+        function sendMoney() {
+
+          const amount = parseInt(numberInput.value);
+
+          let matchedDestination = availableAccounts.find((availableAccount)=>{return availableAccount.userName === destinationAccountName.value});
+
+          if (matchedDestination) {
+            
+            if (!isNaN(amount) && amount > 0 && amount) {
+
+              if (destinationAccountName.value === matchedUser.userName) {
+                feedbackDisplay.textContent = "Cannot send to origination account.";
+              } else {
+
+                if (!isNaN(amount) && amount > parseInt(matchedUser.userBalance)) {
+                  feedbackDisplay.textContent = "Insufficient funds!";
+                } else {
+                  matchedUser.userBalance = parseInt(matchedUser.userBalance) - amount;
+                  matchedDestination.userBalance = parseInt(matchedDestination.userBalance) + amount;
+                  localStorage.setItem("availableAccounts", JSON.stringify(availableAccounts));
+                  feedbackDisplay.innerHTML = `₦${amount} sent to
+                  Name: ${matchedDestination.userName} | Account Number: ${matchedDestination.userAccount}`;
+                  numberInput.value = "";
+      
+                  accountBalanceDisplay.innerHTML = `Balance: ₦${matchedUser.userBalance}`;
+                  accountNumberDisplay.textContent = `Number: ${matchedUser.userAccount}`;
+                }
+
+              }
+
+            } else {
+              feedbackDisplay.textContent = "Amount not enough.";
+              accountBalanceDisplay.innerHTML = `Balance: ₦${matchedUser.userBalance}`;
+            }
+
+          
+          } else {
+            feedbackDisplay.textContent = "Destination account not found.";
+          }
+
+
+        } sendButton.onclick = sendMoney;
+
+  } else {
+    signInFeedback.textContent = "Account does not exist!";
+  }
+
+}signInButton.onclick = signIn;
+
+const signOutFeedback = document.querySelector(".sign-out-feedback");
+
+function signOut() {
+  localStorage.removeItem("loggedInUser");
+  signInFeedback.textContent = "Sign out successful!";
+  window.location.reload();
+  
+}
